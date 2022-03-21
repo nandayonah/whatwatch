05992e314e9efeb089f3829efcb7c481cab33e0a
Fadli Hidayatullah <fadlihdytullah@gmail.com>
2022-03-21 18:14:48 +0700

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Header from './components/Header'
-import Layout from './components/Layout'
 import Home from './pages/Home'
 import MovieDetail from './pages/MovieDetail'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="movies/:id" element={<MovieDetail />} />
