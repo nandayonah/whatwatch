cbc9ee60ec992f2487949a86f0cb0110a89a1167
Fadli Hidayatullah <fadlihdytullah@gmail.com>
2022-03-21 18:45:22 +0700

    Comment unused axios

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+// import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import ErrorAlert from '../components/ErrorAlert'
