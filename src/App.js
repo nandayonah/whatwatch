73531d59db1a5e3fbe74a144b04b227b38888cdc
Fadli Hidayatullah <fadlihdytullah@gmail.com>
2022-03-21 18:14:21 +0700

    Polishing UI with additional Alert and ContentLoader

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Header from './components/Header'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import MovieDetail from './pages/MovieDetail'
+
 export default function App() {
-  return <h1 className="text-9xl font-bold underline">Hello world!</h1>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="movies/:id" element={<MovieDetail />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
---
+++ src/components/LoadingDetail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoadingBlock from './LoadingBlock'
+
+export default function LoadingDetail() {
+  return (
+    <div className="h-screen absolute translate-y-56">
+      <LoadingBlock className="relative -left-4" width={'46rem'} height={300} />
+    </div>
+  )
+}
